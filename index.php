a37c68f6326dda654544d62355aee25bde961fd8
Ipsitt <ipsit2ndacc@gmail.com>
2025-07-24 21:22:00 +0545

    Added sample index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>City Complaint Portal</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+
+<h1><?php echo "Welcome to City Complaint Portal"; ?></h1>
+
+<p>This is a sample PHP + JS test page.</p>
+
+<h2 id="js-output"></h2>
+
+<script>
+    document.getElementById("js-output").innerText = "Hello from JavaScript!";
+</script>
+
+</body>
+</html>
